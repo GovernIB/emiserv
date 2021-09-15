dd17af1af2c9efd6315ce7413e76a9653bacb924
Josep Gayà <josepg@limit.es>
2021-09-15 15:24:41 +0200

    #47 No es mostren els permisos dels serveis

--- emiserv-persistence/liquibase/databasechangelogs/2.0.1/update.sql
+++ emiserv-persistence/liquibase/databasechangelogs/2.0.1/update.sql
@@ -2,7 +2,7 @@
 -- Update Database Script
 -- *********************************************************************
 -- Change Log: db/changelog/db.changelog-master.yaml
--- Ran at: 21/05/21 10:32
+-- Ran at: 15/09/21 15:23
 -- Against: null@offline:oracle?changeLogFile=liquibase/databasechangelog.csv
 -- Liquibase version: 4.3.3
 -- *********************************************************************
@@ -18,4 +18,7 @@ ALTER TABLE ems_usuari DROP COLUMN nif;
 ALTER TABLE ems_usuari DROP COLUMN email;
 
 ALTER TABLE ems_usuari DROP COLUMN inicialitzat;
+
+-- Changeset db/changelog/changes/2.0.1/17.yaml::1618396425533-3::limit
+UPDATE EMS_ACL_CLASS SET class='es.caib.emiserv.persist.entity.ServeiEntity' WHERE class='es.caib.emiserv.core.entity.ServeiEntity';
 
